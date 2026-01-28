be5f49734e4f16f74323535d9f82801c014bb89d
Manuel Caballero <manuel.garciacaballero@merkator.com>
2026-01-28 15:52:07 +0100

    different folder

--- server.js
+++ server.js
@@ -4,10 +4,12 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 8080;
 
-app.use(express.static(path.join(__dirname, 'dist/arbitro-karate')));
+const distPath = path.join(__dirname, 'dist/arbitro-karate/browser');
+
+app.use(express.static(distPath));
 
 app.use((req, res) => {
-  res.sendFile(path.join(__dirname, 'dist/arbitro-karate/index.html'));
+  res.sendFile(path.join(distPath, 'index.html'));
 });
 
 app.listen(port, () => {
